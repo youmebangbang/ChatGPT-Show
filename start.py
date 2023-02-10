cbafbc9c4f468a88bc05f3a99383c8de614ff10a
Youmebangbang <34688253+youmebangbang@users.noreply.github.com>
2023-02-09 21:08:58 -0500

    Add files via upload

--- start.py
+++ start.py
@@ -48,8 +48,10 @@ class Control:
         self.label = None
         self.title_label = None
         self.video_title = ""
-
+        
         self.start_time = 0
+        self.session_token = None
+        self.offline = False
 
         self.taco_model = None
         self.waveglow_model = None
@@ -74,30 +76,57 @@ class Control:
         print("starting timer at {}".format(time.time()))
         self.start_time = time.time()  
 
-    def run(self):
+    def run(self):        
         while True:
-            root.update_idletasks()
-            root.update()
-                    
-            if self.keyword_reset:
-                i = len(organisms)
-                self.keyword = organisms[random.randint(0, i-1)]
-                self.keyword_reset = False            
+            # self.offline = True
+            if not self.offline:
+                root.update_idletasks()
+                root.update()
+                        
+                if self.keyword_reset:
+                    i = len(organisms)
+                    self.keyword = organisms[random.randint(0, i-1)]
+                    self.keyword_reset = False            
 
-            if not self.get_video_ready and not self.get_video_running:
-                self.get_video() #threaded
-            
-            if not self.get_chatgpt_text_ready and not self.get_chatgpt_text_running:
-                self.get_chatgpt_text("a 5 paragraph text on {}, narrated by david attenborough. use some funny nature jokes.".format((self.keyword).strip())) #threaded
+                if not self.get_video_ready and not self.get_video_running:
+                    print("ONLINE: RUNNING GET VIDEO")
+                    self.get_video() #threaded
+                
+                if not self.get_chatgpt_text_ready and not self.get_chatgpt_text_running:
+                    print("ONLINE: RUNNING CHATGPT") 
+                    self.get_chatgpt_text("a 5 paragraph text on {}, narrated by david attenborough. use some funny nature jokes.".format((self.keyword).strip())) #threaded
 
-            if not self.is_inference_ready and self.get_chatgpt_text_ready and not self.is_inference_running:
-                self.run_inference() #threaded
+                if not self.is_inference_ready and self.get_chatgpt_text_ready and not self.is_inference_running:
+                    print("ONLINE: RUNNING INFERENCE")
+                    self.run_inference() #threaded
 
-            if self.get_video_ready and self.get_chatgpt_text_ready and self.is_inference_ready and not self.is_play_audio_running:                
-                self.play_audio() # threaded
-                time.sleep(2)
-                self.play_main_video()      
-            time.sleep(1)
+                if self.get_video_ready and self.get_chatgpt_text_ready and self.is_inference_ready and not self.is_play_audio_running:       
+                    print("ONLINE: STARTING SHOW")          
+                    self.play_audio() # threaded
+                    time.sleep(2)
+                    self.play_main_video()      
+                time.sleep(1)
+
+            else:
+                root.update_idletasks()
+                root.update()
+                #offline mode, get stuff from files
+                
+                if not self.get_video_ready and not self.get_video_running:
+                    print("OFFLINE: RUNNING GET VIDEO")
+                    self.get_video() #threaded
+                
+                if not self.is_inference_ready and not self.is_inference_running:
+                    print("OFFLINE: RUNNING INFERENCE")
+                    self.run_inference() #threaded
+
+                if self.get_video_ready and self.is_inference_ready and not self.is_play_audio_running: 
+                    print("OFFLINE: STARTING SHOW")               
+                    self.play_audio() # threaded
+                    time.sleep(2)
+                    self.play_main_video()      
+                time.sleep(1)
+
             
     def play(self):
         #copy ready video into main video
@@ -116,11 +145,12 @@ class Control:
             play_obj = wave_obj.play()
             play_obj.wait_done()
         self.is_audio_playing = False
+
         #restart setup
-        while True:
-            if self.get_chatgpt_text_ready and self.get_video_ready and self.is_inference_ready:
-                break
-            time.sleep(0.5)
+        # while True:
+        #     if self.get_chatgpt_text_ready and self.get_video_ready and self.is_inference_ready:
+        #         break
+        #     time.sleep(0.5)
 
         self.is_inference_ready = False
         self.is_inference_running = False
@@ -150,6 +180,20 @@ class Control:
         t_inf.start()
 
     def get_video(self):
+        if self.offline:         
+            i = 0
+            while True:
+                filename = os.path.join("texts/", str(i) + ".txt")
+                if not os.path.exists(filename):
+                    break
+                i += 1
+            r = random.randint(0, i)
+            f = "texts/{}.txt".format(r)
+            print("RUNNING VIDEO SEARCH OFFLINE: GETTING FILE {}".format(f))
+            with open(f, 'r') as f:
+                self.keyword = f.readline().strip()
+                self.chatgpt_message = f.read().strip()
+
         self.get_video_running = True
         t_inf = threading.Thread(target=self.get_video_t, args=())
         t_inf.start()        
@@ -176,8 +220,6 @@ class Control:
                 results = videosSearch.result()
                 filtered = []
                 for x in results['result']:
-                    print("x var")
-                    print(x)
                     if not "none" in x['duration']:
                         d = x['duration'].split(':')[0]
                         if int(d) <= 10 and int(d) >= 3:    
@@ -210,34 +252,73 @@ class Control:
     def get_chatgpt_text_t(self, text):
 
         #check timer for rate limit
-        while (time.time() - self.start_time) < 240:
+        while (time.time() - self.start_time) < 300:
             print("Waiting for rate limit timer")
             print((time.time() - self.start_time))
             time.sleep(5)
         self.start_timer()
 
         session_token = 'eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..YXVZD6ZXGxJvJGx8.jj-7qK49XFPZK1Q29ijjTateXfkv1TAW69h70f37Bw_9_qpTtmWcCZJJNwWQE-1C4lXZ8SwnGM-N93yaZv5nw_KJ2VTk2ImeAczmg7kQxulik_49dtJe_0L8LqHe37zbA8LCw9DsOcBvr72a82Sr8Zp_hJXcsSWwP5wi-QZet4ubHK9DDfgskRSJeu2okexFJPgm4Fcus717gtAeCpWyXURLtlZ9lk7HAiF16f0O7oNFJVpC2IGZcf0B9tc3yfJRvvrKJOcJmdE5dwGuIV9ldoaey6Vd9buUYwYAxHUEmxQY5FTBhYsRY4qnzG-sIrpPFO2HI6yFLp9I8At-34qn2szEdfMHiBhoOANmic1qOgBepqefDvw6N9m-1NM12zlcSSuLD86tePL_9H_iLkEjMfDVGl_KDjRKaBkYlb_N8Qk-8ri1VqG6PuJnxUtMhe1THQC9V_5cpY-NkFr2-JrZ5nIR-wSFO4nAPbZM9cDSiMjHj95KjkIwrm7mcy2GI7sXfXN1b-tOoEx5highi8J_f0cfd9LYahbAy465ko-iB4YOg_ImMHn4QtCQI_5ZbCNU-w6P-FtbBEqF2En_4X2uF8BfnRlidwjoq2sGmvm1HrZ9iGs5dzE7G9HgvMD5cN-T7-HpdnjSiX1YVOcxobdjcwF0LzxZ5_qYorW4lGAmhUVBmvlcSEEiI4avT4Cioy5yWZMM0y1rSjyx-kEjfN6br2JA6yz7zmQD5_fs67dnt7g6u9q0AzGkZ_WRUp_9D4RsQWFZNds4akCO2qznPfGPtAWaJNKTcryrgDEWPaj1NcP5Sw3GAjNtvtUvBeqm63gYBZwXb8HtN_oZcRRkGuRvxECRpj25NTzj3ZVIerua-38cfIF7U18FTmSr30VzqrKHtycm0xwrrp50fC1r9ln6m34MSSxm7KBybcL3I9C23CvuJxZPjUu7u-0_5dKa6fX9Qdgb8ojmZOYBzDQvjvyGGD4rVmlPp60dpug_mWRzlwawHt9_cEO0ZWba_3nAq15H19LIjn3V17E44HWiww3gpDjB6gOUr6TMRnVMeGoEyQqbrNULkUk4OsHFAlf1bFCyto5hHSzWhUDonQR3pGMFaurgt1Vp22NP08gcRRMlOU9Oe11Q7ZjKiuWFbKUtXGPmb2NHVP_cgIPLNrAweoSI_6i7qNx-BZMSA9c2SFLDC8tSfAVZ0anIGWdYtjIL5SOWexGcMsXWeANjpyyWO3LdO5uEVK2UtMOSgJznsoNA0mCtMZwXTzaB8zuB4-SzpZYK-fqTNLL7_ZIo8XyI8kuzlDuUHwBsV_XtGVxfWKeNJdJI0lhYxcyF1kS96EXURHWlMzncG7gVQ83wycPIy9ZrihUZIllZtjz40oZAkTzRVMinT5W2p9oGTeFlrqsbUdUW5FDULahN8n4E9ML0gGFTicAfAGQextJWTHruG3Bn-87DG_0YkWA6Pz_nkV0K8-uPG7Osw4P8jTRbrRNZAb9qEZUJqYneQksPKFxJV4SlFnJ9IIyNuAasgV9mKVheeBrAy41FJhKWNFpIv6bgAMKMkRG1Ndl0ExusKwlhtGMpA9zqKcQqaPW53-Euh7qR5aeFUVc6BddGHa0Kpn7VVvYOTFdxH_WfJwwjbBWXy5BjOU2g6O_yG8WE7VC-FDYcZaWAnFuNSR76EoVOjGsiV3xxdpLX_Z1qfB22w723NCNcmMxo9Vw3W0PK5q57JAqC7B1inXrln45DsmK2ZF5E0meR0bmiqnIdYtwmjg7Kt-naJUmbfQU8o-zw03QMUqH62n_8fn0pKKCYPvmGpi0vZRQOl8-jOfM-CnO_YJfXQQMqLuwtavjDXvQ5HBhZgAYjsz4s3RbidBg3mTu-u1zqlX5Q0i3G79LWYwCCgVZ3CFuiRjNHsa3lwDhYhZ9njY_u7tkER_J1I2z9kjWpZVx9PVI3ZqrOZKQkRjDjymG6s1eFdDpEQM-krwmP_FLvhyfDK4QrrxwyJagAFs8uJxLgEXMba7gwOOz5iuS5aXUfypWkvMdTOwrj3dWFgajQ-BdbAT7Qpt0XWFwdXstqkkGCmM4JHRkhAM3FY_wP3LBTinXb0CkMP8odMlIVJtKmALGBVX03xjCivQHEXA78DOFO15wZc2A0M3NMsrYmHPE4KvwqjFmcrV7iv5IPsRRwFxZ6WbA0tkAOFnMymwrh9dcYmd8sYqd465zEj0eCyumBbVOtMPvvU_NVkIC44GXnOa9mq0HOQ6FW7cXDGvhKAz6zQuOVeSgcTMKEEd18fYk-k0K9guov1BcdxjqfkKSnl2KZr0rRuKyyz0HDQ3LIkErMos2r-u1-LRRxB76F_SvvcO7YrwOizD-5_l7JOiSgjSYoavIZJOAxyyZ9dZerIxupOW9I0e49cSOTq2q3HsDw9dpad2QjT8Gl6Rw2431Ll4-yAxyOOTpo8EHC6XPgoDGakH_RTAGxIIKSYCZAKQeNRxWQhNkhzaFfGjkqnqNwQpPhx5jKSGH63uha-XxUDYc6-yKxBhVOQfRgpVNmsokqTuR1JXFg59FTAs1hpzgKXp_RDkximSIqS55w68E7YQ2h-Hk6_7Kyi5JdR8hfoE5GWEoCGOWiuH-zFlBqmnXcLHDscrNuWxKwjpzEc0F0gF0_0ZXZIQSVY9Lq0jimiDExXjho-ZudZX2SjltX98AWioNotbLPjWN-MuqwZfAMa5lU1jQUoMDB.StA2mwwRYI8Trwy0i580-Q' 
+
+        session_token2 = 'eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..LPt9-KrzwiR_E-50.Lg9jUfcn5bs3UeCH7RQ1XvsdLnBwkGGPIk9y7ZU92YizZUhB0c5LP3qSZh6fWT_VNvW9ntC59Y_JM8lwE2E-JESJJJjMvnumdTdgyrhyKEOdgW-pDlbvjq5uFvqLs5RUT6tZYn_H1yETtAnMfoSBF87PApdYNLRFWpp1MrWMI1ThC2RrK0h3NNVQdBJIy2QogRbooeAx8KFncay05QepZVuEL8Mr_2-AV7_4pZ7wZHZxMnx7KSCTFyTerXeEiL86TZXIPOsqF0zd0LgcWgFxjeW9g-rjqGpBUitYlhEylUMQmX_L_xRMGiCAChgdpziqFeuIxSQSJEWfFxbCNVrwKNmKNPCRZ2CWsOycgGf2S2xP4PzNRRiUmmIOBD2yF463ZvYhDW9EIs34-C02MhT-vBXLVo0LPs_EJgMYiJbMtTYJs3AaudwRNkbqQHwkhulpbxvK27RvI8lhqWi_0Nvu40eprfFjWqFhCtCwEl4pJxTUvr1I9X2sy3QFy6UL2FM1D7EJHio2vZyXq1AF9SjbdEe6bEvFGGiypZFh-Axk6RLPhavtVpKhTsc2AN6d9ut7K3i-vfTOifG-PsQMRO0T8cWOgtv4DoE3M7D63lDmPkirPvKS_yZLzjwtl2kGYqq_wXkH545EVoMYmB1CSQMpijw_QcQbEmx2BhNcyNN3cKtlC90YqPWWA41eFg0zyugrJjlpwHof_ljD92EvmUI0pufvWe9qrtTN6TGG21Mie0fOBG1WLsg-9d6j0IoH-B7a3-_M6a_Zsv4Tb9z1ohKYz9-Yv00CU4i6mjv2Fk43dY_1cNNEqRWxnOygYkquODAgPnzzEvq72pLONdnodpmsVV_FNyWUIGhN4UjgVhOgODJAieAV7XCdVWTex2TMAJngkrh0n-pHsTV6SQnUTCOkHa3mXmK-i4TZhugEFJYZdDweSheWgL_DiOrGv8gXZdEQiTR1i9BiqD2R43Xwe0pKcisxUqG0kKQuQ-QGlTsLgkUAqKOou9nsiv1ixuX_LTRvwQMXC716YQOdm-EFglvE5NMAf1da4sERSjHNHrJgSbtpyUPpi9nxhtUVKRCBxPbKhdBDjVSNymOmZaM-QdwKO9zSWTJHWw0SrIeGqsxpm1vJvwUpeDPGUc-Zfs75mwkTiBklwbew1y4JeWYSiIHrM1gCzuffs1fmDuN2TECz8NOEXVARucYdKhPMUEWIfX1LDFH2ow1eUbX5rnNDg0WbBQumd4WlvOhuDLqU5QCYMAFPlCmXo5Ko6uwj3o8nv4Jx-Kw3trIfA-gQ74QigRpM2CAcXLBTs8SrE6tF0YqgP1K3tS4XsNYZ-fZ2iJmcunKODBVUKGq6J6im591BKKB54zW7X2Vq-6ntkxU4ECPAlp_p1Q8W5zCwP1w7nsDaJwtL_YXacdkj4EXdgrACv-Z40ELRSQUBR7duotNgWljTpoHl7m0wWO7WZcejW2YZcuvm7gvTiH9Fya7khmuUMZFmOsK9soGnCW3kgMN0dIUwlNZi-w7HEy1EVhqZQtXMa63GceyMJ2KtGxtMAeWIDq_PylE9Tnkw9yumQP8rlGwTdK6ZKoai2_oaONeAWD_hlyJK8-vRm-7faVnLGDG5W24s_2uQnGEUyQzsXhRq5QyR608dH_KLM-tj_VldglC_WBL9VWpMb7mpp3nYrb4M2jAaf0Rgeak35RtigNNneP8Zp5fHYRiRMM1WrtFDeenq4DocjQwcsCAcQQSXy3zD7BHosuXmV0c3A7h7jFoWDK7u7u7VjRxsAFkRD9CLwZ02b9LspYl7Cqx9EPfNp7nqNGNJ5jwWSGs2dl1GObf7iiRqMjUlT6pZTsXUOKcxtZS8pq0y8lxH7sRdcytW9YGVisYQEIj2sQxQrKkRqSMk9j-7XB1CNOsDjqU4AuQaGDG42z_Dn_dPSHoaM6CZwggOzWvzehY7_G0xvtUb9nizYTqu-khae3B8lizVk35Mgro9WbuUBcxi7nhbHQ7_P9cNksUv_YaZYXInzTAYMWknI4zYqB3GwyHl39bixNbrrPFWyuJ9pqgNKyTOHHjKxOKf0Uo-lVwsxARUYf8qLT3lCw7xtwclVrHeYmzo5Wtu4jUi7xQyy1bXknXk57mYFyVvUc4KEYbpVr43YBm1uvtWLsv6T9CJDGzrW8lN4clvD4YfMMya2Io-2C-atnvNy6BMJrpzm_AqGu22Tq4qmksCoXTDI8d2p7z6w7q6dJxqaz8FjWJHPVa5bIjsIS0vyDOnDNpwu7CGGhnexL5JKKXaparFUFaOgM-5M3Kc2hXVPJQQXM2RlmJjmx4Dk5Jkbnd-i2N5DMk0T25TC_tn1Gir7tIDb_3gmiY-2Lm6-3M3TkzeHNetZJl_ABQq8j_k73jBluC-xOOIils_EhWhRPJTnljeFAGJLpsaDLkUfJdfu1O7Z2SQtHpJO-ndBN--pZBZJAgALhmR5LMELIIwT7iDo_rTd0yJaLk-OZl7yDf2WLVHADuIFEWeObDSGwIzx40fvd8UCoiA8jZWq24G6GqFN62AOdxn06AIjiqoCklKplKWTJ7KLaF4zJW0-2mOgOSFrbBCj8mJCClirYBNVECpZya_ZSmcJHBK0fvnFfpnGNl5srAIUv5lHC66HQGGvfJnyZ_ZAKkykFayslHdpN-x42J2TxfsEdk3nKp8RCi7SQ4.IODKzeMLPwryDWll0Al6fA'
+
+        if not self.session_token:
+            self.session_token = session_token
+
+        if self.session_token == session_token:
+            self.session_token = session_token2
+        else:
+            self.session_token = session_token
         
         
-        api = ChatGPT(session_token)
+        api = ChatGPT(self.session_token)
         
         while True:
             try:
                 print("Sending GPT message")
                 t = api.send_message(text)
+                ####
             except:
+                print("RUNNING OFFLINE")
+                self.get_chatgpt_text_ready = False
+                self.get_chatgpt_text_running = False
+                if self.session_token == session_token:
+                    self.session_token = session_token2
+                else:
+                    self.session_token = session_token
+                self.offline = True
                 api.__del__()
-                api = ChatGPT(session_token)
-                # page crapped out, time to reload
+                api = ChatGPT(session_token)      
             else:
+                self.offline = False
                 break
-            time.sleep(10)
+            time.sleep(60)
+            # self.offline = True
+            # self.get_chatgpt_text_ready = False
+            # self.get_chatgpt_text_running = False
+            # print("RUNNING OFFLINE")
+            # time.sleep(60)
 
         #api.reset_conversation()
         self.chatgpt_message = t['message']
         # t = "Good day to all, it's David Attenborough here. Today, I would like to talk to you about one of the most fascinating creatures in the animal kingdom - the Wombat. Wombats are short-legged, sturdy and heavy creatures with a reputation for being slow-moving and docile."
 
         #self.chatgpt_message = t
+
+        #write message to file
+        subdir = "texts"
+        if not os.path.exists(subdir):
+            os.makedirs(subdir)
+        i = 0
+        while True:
+            filename = os.path.join(subdir, str(i) + ".txt")
+            if not os.path.exists(filename):
+                break
+            i += 1
+        filename = "texts/{}.txt".format(i)
+        with open(filename, 'w') as f:
+            f.write("{}\n".format(self.keyword))
+            f.write(self.chatgpt_message)
 
         api.__del__()
         self.get_chatgpt_text_ready = True
@@ -287,11 +368,18 @@ class Control:
             text = text.strip('”')
             text = text.strip('“')            
             text_list = re.split(r'(?<=[\.\!\?])\s*', text)
-            text_list = [i for i in text_list if i]
-            print(text_list)
+
+            #remove blank and short cuts
+            text_list_cleaned = []
+            for i in text_list:
+                if i and len(i) > 10:
+                    text_list_cleaned.append(i)
+
+            # text_list = [i for i in text_list if i]
+            print(text_list_cleaned)
             wav_list = []
 
-            for i, t in enumerate(text_list):
+            for i, t in enumerate(text_list_cleaned):
                 os.makedirs("wav_out", exist_ok=True)
                 wav_file = "wav_out/out{}.wav".format(i)        
                 sequence = np.array(text_to_sequence(t, ['english_cleaners']))[None, :]
